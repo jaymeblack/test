e886732ee8eb5e6436165cba804425324ec66f35
unknown <jaymeblack@gmail.com>
2017-10-25 15:11:44 -0500

    yep

---
+++ DogClicker/Assets/LesserDogBehavior.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LesserDogBehavior : MonoBehaviour 
+{
+	public GameObject head;
+	public GameObject neck;
+	public GameObject body;
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update(){
+		if(Input.GetKey(KeyCode.Space))
+		{
+			head.SetActive(false);
+		}
+		else
+		{
+			head.SetActive(true);
+		}
+	}
+	void OffScreenBehavior()
+	{
+		
+	}
+
+}
